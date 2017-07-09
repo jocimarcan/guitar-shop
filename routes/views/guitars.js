e9f72b0e42622b6fbd55d4d9352343028b071576
David Daza <03dazal@gmail.com>
2017-07-09 16:01:34 -0500

    Pagination added in guitars view

--- routes/views/guitars.js
+++ routes/views/guitars.js
@@ -9,28 +9,43 @@ exports = module.exports = function(req, res) {
 	locals.brands = Guitar.fields.brand.ops
 	locals.data = {
 		guitars: [],
+		pages: [],
 		brand: []
+	}
+
+	var brand = req.query.brand ? req.query.brand : null
+	var pageOps = {
+		page: req.query.page || 1,
+		perPage: 9,
+		filters: brand ? { 'brand': brand } : null
+	}
+
+	var fetch = function (query, callback) {
+		query.exec(function(err, guitars) {
+			locals.data.guitars = guitars.results
+			locals.data.brand = brand
+			locals.data.pages = {
+				total: guitars.totalPages,
+				actual: guitars.currentPage,
+				previous: guitars.previous,
+				next: guitars.next
+			}
+			callback(err)
+		})
 	}
 
 	view.on('get', { page: '1' }, function(next) {
 		if (req.query.brand) {
-			q = Guitar.model.find({ brand: req.query.brand })
-			q.exec(function(err, guitars) {
-				locals.data.guitars = guitars
-				locals.data.brand = req.query.brand
-				next(err)
-			})
+			var q = Guitar.paginate(pageOps).where('brand', req.query.brand)
+			fetch(q, next)
 		} else {
 			res.redirect('/guitars')
 		}
 	})
 
 	view.on('init', function(next) {
-		var q = Guitar.model.find()
-		q.exec(function(err, guitars) {
-			locals.data.guitars = guitars
-			next(err)
-		})
+		var q = Guitar.paginate(pageOps)
+		fetch(q, next)
 	})
 
 	view.render('guitars')
